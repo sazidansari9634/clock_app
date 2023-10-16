1b824ba3d41126755e6924548f24c29539512acf
Sazid Ansari <sazidansari9634@gmail.com>
2023-10-17 05:34:56 +0545

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:clock_app/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Clock App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
